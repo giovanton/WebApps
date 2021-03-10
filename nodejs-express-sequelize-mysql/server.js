0ff7990c470c6b5a8b3f0009825ff136c7bf5f1d
giovanton <giovantong@gmail.com>
2021-03-10 19:47:54 +0100

    priemros pasos datos

--- nodejs-express-sequelize-mysql/server.js
+++ nodejs-express-sequelize-mysql/server.js
@@ -18,15 +18,24 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 
 const db = require("./app/models");
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
     console.log("Drop and re-sync db.");
 });
 
 require("./app/routes/user.routes")(app);
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to gioserver application."});
+    res.json({ message: "Welcome to GioServer application."});
 });
 
 // set port, listen for requests
